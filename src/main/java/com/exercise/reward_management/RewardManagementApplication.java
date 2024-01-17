dda2516dbe7db6de7929ca4dfa8ca026ada58c6b
arabica_TrungPH <arabica_2@bitel.com.pe>
2024-01-17 17:18:12 +0700

    base

---
+++ src/main/java/com/exercise/reward_management/RewardManagementApplication.java
@@ -0,0 +1,13 @@
+package com.exercise.reward_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RewardManagementApplication.class, args);
+    }
+
+}
